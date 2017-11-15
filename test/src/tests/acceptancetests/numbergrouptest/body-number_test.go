cfcdd7c2fd76bf6c5700da92313e7f70c54399be
Joel Hendrix <jhendrix@microsoft.com>
2017-11-15 10:04:54 -0800

    Remove recently deleted tests.

--- test/src/tests/acceptancetests/numbergrouptest/body-number_test.go
+++ test/src/tests/acceptancetests/numbergrouptest/body-number_test.go
@@ -109,11 +109,6 @@ func (s *NumberSuite) TestPutSmallFloat(c *chk.C) {
 	c.Assert(err, chk.IsNil)
 }
 
-func (s *NumberSuite) TestGetInvalidDecimal(c *chk.C) {
-	_, err := numberClient.GetInvalidDecimal()
-	c.Assert(err, chk.NotNil)
-}
-
 // func (s *NumberSuite) TestGetBigDecimal(c *chk.C) {
 // 	res, err := numberClient.GetBigDecimal()
 // 	c.Assert(err, chk.IsNil)
