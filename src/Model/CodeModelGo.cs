78f199b0ecd07cb39e5e5f9bc8b33e50605ded63
Joel Hendrix <jhendrix@microsoft.com>
2018-05-31 14:15:18 -0700

    Fix future/pageable types.
    The previous LRO changes were incomplete, they missed the case where
    futures intersect with pageable types.
    Avoid importing the net/http package when it's not needed.

--- src/Model/CodeModelGo.cs
+++ src/Model/CodeModelGo.cs
@@ -137,10 +137,19 @@ namespace AutoRest.Go.Model
                 {
                     imports.Add(PrimaryTypeGo.GetImportLine("github.com/Azure/go-autorest/autorest"));
                 }
-                if (ModelTypes.Any(mt => mt is FutureTypeGo))
+                var futureModels = ModelTypes.Where(mt => mt is FutureTypeGo).Cast<FutureTypeGo>();
+                if (futureModels.Any())
                 {
                     imports.Add(PrimaryTypeGo.GetImportLine("github.com/Azure/go-autorest/autorest/azure"));
-                    imports.Add(PrimaryTypeGo.GetImportLine("net/http"));
+                    // if any of the futures return the non-default response then we need the net/http package
+                    foreach (var ftg in futureModels)
+                    {
+                        if (!ftg.IsDefaultReturnType)
+                        {
+                            imports.Add(PrimaryTypeGo.GetImportLine("net/http"));
+                            break;
+                        }
+                    }
                 }
                 ModelTypes.Cast<CompositeTypeGo>()
                     .ForEach(mt =>
