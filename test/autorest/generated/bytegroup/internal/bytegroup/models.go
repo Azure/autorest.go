36ae5efc85e5063a659ecde7c09c16a753346491
Joel Hendrix <jhendrix@microsoft.com>
2020-01-14 09:38:12 -0800

    refactor test directory layout (#196)
    added test for GetEmpty
    fixed unmarshalling bug

--- test/autorest/generated/bytegroup/internal/bytegroup/models.go
+++ test/autorest/generated/bytegroup/internal/bytegroup/models.go
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-package generatortests
+package bytegroup
 
 import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
