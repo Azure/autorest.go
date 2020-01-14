91da355e80a9cc6900a270f03dca4e7a7aba4cf2
Catalina Peralta <caperal@microsoft.com>
2020-01-13 17:29:57 -0800

    Adding ByteGroup acceptance code

---
+++ test/acceptance/bytegroup/go.mod
@@ -0,0 +1,11 @@
+module bytegroup
+
+go 1.13
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.1.0
+	github.com/Azure/go-autorest/autorest v0.9.3
+	github.com/Azure/go-autorest/autorest/validation v0.2.0
+	github.com/Azure/go-autorest/tracing v0.5.0
+
+)
---
+++ test/acceptance/bytegroup/models.go
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package bytegroup
+
+import azinternal "bytegroup/internal/bytegroup"
+
+// ByteArray to consume byte information
+type ByteArray = azinternal.ByteArray
