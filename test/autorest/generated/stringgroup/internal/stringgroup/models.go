6f43a821ee50130ef537429ad0a7487e8facef60
Catalina Peralta <caperal@microsoft.com>
2020-01-16 16:30:25 -0800

    Added string group reference samples for string client GetMBCS and PutMBCS endpoints

---
+++ test/autorest/generated/stringgroup/internal/stringgroup/models.go
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package stringgroup
+
+import "github.com/Azure/azure-sdk-for-go/sdk/azcore"
+
+// Error ...
+type Error struct {
+	Status  int32
+	Message string
+}
+
+func newError(resp *azcore.Response) error {
+	err := Error{}
+	if err := resp.UnmarshalAsJSON(&err); err != nil {
+		return err
+	}
+	return err
+}
+
+func (e Error) Error() string {
+	return e.Message
+}
+
+// GetMBCSResponse ...
+type GetMBCSResponse struct {
+	StatusCode int
+	Value      []string `json:"value,omitempty"`
+}
+
+// PutMBCSResponse ...
+type PutMBCSResponse struct {
+	StatusCode int
+}
---
+++ test/autorest/stringgroup/stringgroup_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package bytegrouptest
+
+import (
+	"context"
+	"generatortests/autorest/generated/stringgroup"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func getStringClient(t *testing.T) *stringgroup.StringClient {
+	client, err := stringgroup.NewStringClient(stringgroup.DefaultEndpoint, nil)
+	if err != nil {
+		t.Fatalf("failed to create string client: %v", err)
+	}
+	return client
+}
+
+func deepEqualOrFatal(t *testing.T, result interface{}, expected interface{}) {
+	if !reflect.DeepEqual(result, expected) {
+		t.Fatalf("got %+v, want %+v", result, expected)
+	}
+}
+
+func TestGetMBCS(t *testing.T) {
+	client := getStringClient(t)
+	result, err := client.GetMBCS(context.Background())
+	if err != nil {
+		t.Fatalf("GetMBCS: %v", err)
+	}
+	expected := &stringgroup.GetMBCSResponse{
+		StatusCode: http.StatusOK,
+		Value:      []string{"啊齄丂狛狜隣郎隣兀﨩ˊ〞〡￤℡㈱‐ー﹡﹢﹫、〓ⅰⅹ⒈€㈠㈩ⅠⅫ！￣ぁんァヶΑ︴АЯаяāɡㄅㄩ─╋︵﹄︻︱︳︴ⅰⅹɑɡ〇〾⿻⺁䜣€"},
+	}
+	deepEqualOrFatal(t, result, expected)
+}
+
+func TestPutMBCS(t *testing.T) {
+	client := getStringClient(t)
+	result, err := client.PutMBCS(context.Background(), []string{"啊齄丂狛狜隣郎隣兀﨩ˊ〞〡￤℡㈱‐ー﹡﹢﹫、〓ⅰⅹ⒈€㈠㈩ⅠⅫ！￣ぁんァヶΑ︴АЯаяāɡㄅㄩ─╋︵﹄︻︱︳︴ⅰⅹɑɡ〇〾⿻⺁䜣€"})
+	if err != nil {
+		t.Fatalf("PutNonASCII: %v", err)
+	}
+	expected := &stringgroup.PutMBCSResponse{
+		StatusCode: http.StatusOK,
+	}
+	deepEqualOrFatal(t, result, expected)
+}
