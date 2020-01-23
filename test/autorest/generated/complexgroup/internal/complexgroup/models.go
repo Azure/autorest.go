cace26dd7f48de0c3b24a5eb8121cbd527d2d5c8
Catalina Peralta <caperal@microsoft.com>
2020-01-23 15:09:20 -0800

    Adding more body complex endpoints for the basic model and initial endpoints for primitive models

--- test/autorest/generated/complexgroup/internal/complexgroup/models.go
+++ test/autorest/generated/complexgroup/internal/complexgroup/models.go
@@ -49,6 +49,31 @@ type Basic struct {
 	Color *ColorType `json:"color,omitempty"`
 }
 
+// IntWrapper ...
+type IntWrapper struct {
+	Field1 *int32 `json:"field1,omitempty"`
+	Field2 *int32 `json:"field2,omitempty"`
+}
+
+// DoubleWrapper ..
+type DoubleWrapper struct {
+	Field1                                                                          *float64 `json:"field1,omitempty"`
+	Field56ZerosAfterTheDotAndNegativeZeroBeforeDotAndThisIsALongFieldNameOnPurpose *float64 `json:"field_56_zeros_after_the_dot_and_negative_zero_before_dot_and_this_is_a_long_field_name_on_purpose,omitempty"`
+}
+
+// BooleanWrapper ...
+type BooleanWrapper struct {
+	FieldTrue  *bool `json:"field_true,omitempty"`
+	FieldFalse *bool `json:"field_false,omitempty"`
+}
+
+// StringWrapper ...
+type StringWrapper struct {
+	Field *string `json:"field,omitempty"`
+	Empty *string `json:"empty,omitempty"`
+	Null  *string `json:"null,omitempty"`
+}
+
 // GetValidResponse ...
 type GetValidResponse struct {
 	StatusCode int
@@ -87,5 +112,23 @@ type GetNotProvidedResponse struct {
 // GetIntResponse ...
 type GetIntResponse struct {
 	StatusCode int
-	Basic      *Basic
+	IntWrapper *IntWrapper
+}
+
+// PutIntResponse ...
+type PutIntResponse struct {
+	StatusCode int
+	IntWrapper *IntWrapper
+}
+
+// GetLongResponse ...
+type GetLongResponse struct {
+	StatusCode int
+	Value      int64
+}
+
+// PutLongResponse ...
+type PutLongResponse struct {
+	StatusCode int
+	Value      int64
 }
