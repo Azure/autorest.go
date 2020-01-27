d0395c7f0233e4194f24a6c7504b4c46a44e3fb2
Joel Hendrix <jhendrix@microsoft.com>
2020-01-27 09:43:37 -0800

    initial work for generating models (#211)
    * initial work for generating models
    
    foundation for generating internal/models.go files.  it is incomplete
    
    * tweak CI

--- src/namer/namer.ts
+++ src/namer/namer.ts
@@ -3,45 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { deserialize, serialize, camelCase } from '@azure-tools/codegen';
+import { serialize, pascalCase } from '@azure-tools/codegen';
 import { Host, startSession, Session } from '@azure-tools/autorest-extension-base';
 import { codeModelSchema, CodeModel, Language } from '@azure-tools/codemodel';
 import { visitor, clone, values } from '@azure-tools/linq';
 
-// the namer takes a code model in 
-// and writes a code model out.
-// 
-
-function cloneLanguageInfo(graph: any) {
-  // make sure recursively that every language field has Go language info
-  for (const { index, instance } of visitor(graph)) {
-    if (index === 'language' && instance.default && !instance.go) {
-      instance.go = clone(instance.default, false, undefined, undefined, ['schema', 'origin']);
-    }
-  }
-}
-
-export async function process(session: Session<CodeModel>) {
-  const model = session.model;
-
-  if (model.language.go) {
-    // this looks like it already has data for this model.
-    // send back an error
-    session.error('bad flavor', ['go:1000', 'already-processed'], model.language.go);
-    throw Error('Go Namer Failed');
-  }
-
-  // copy all the .language.default data into .language.go
-  cloneLanguageInfo(model);
-
-  // go set the names of things you care about
-  for (const schema of values(model.schemas.objects)) {
-    const details = <Language>schema.language.go;
-    details.name = camelCase(details.name);
-  }
-  return session;
-}
-
+// The namer creates idiomatic Go names for types, properties, operations etc.
 export async function namer(host: Host) {
   const debug = await host.GetValue('debug') || false;
 
@@ -58,5 +25,57 @@ export async function namer(host: Host) {
       console.error(`${__filename} - FAILURE  ${JSON.stringify(E)} ${E.stack}`);
     }
     throw E;
+  }
+}
+
+async function process(session: Session<CodeModel>) {
+  const model = session.model;
+
+  if (model.language.go) {
+    // this looks like it already has data for this model.
+    // send back an error
+    session.error('bad flavor', ['go:1000', 'already-processed'], model.language.go);
+    throw Error('Go Namer Failed');
+  }
+
+  // copy all the .language.default data into .language.go
+  cloneLanguageInfo(model);
+
+  // pascal-case names of objects and their fields
+  for (const obj of values(model.schemas.objects)) {
+    const details = <Language>obj.language.go;
+    details.name = pascalCase(details.name);
+    for (const prop of values(obj.properties)) {
+      const details = <Language>prop.language.go;
+      details.name = pascalCase(details.name);
+    }
+  }
+
+  // pascal-case operation groups and their operations
+  for (const group of values(model.operationGroups)) {
+    const details = <Language>group.language.go;
+    details.name = pascalCase(details.name);
+    for (const op of values(group.operations)) {
+      const details = <Language>op.language.go;
+      details.name = pascalCase(details.name);
+    }
+  }
+
+  // fix up enum type and value names
+  for (const enm of values(session.model.schemas.choices)) {
+    for (const choice of values(enm.choices)) {
+      const details = <Language>choice.language.go;
+      details.name = `${enm.language.go?.name}${pascalCase(details.name.toLowerCase())}`;
+    }
+  }
+  return session;
+}
+
+function cloneLanguageInfo(graph: any) {
+  // make sure recursively that every language field has Go language info
+  for (const { index, instance } of visitor(graph)) {
+    if (index === 'language' && instance.default && !instance.go) {
+      instance.go = clone(instance.default, false, undefined, undefined, ['schema', 'origin']);
+    }
   }
 }
---
+++ src/transform/transform.ts
@@ -0,0 +1,117 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { serialize } from '@azure-tools/codegen';
+import { Host, startSession, Session } from '@azure-tools/autorest-extension-base';
+import { ArraySchema, codeModelSchema, CodeModel, Language, SchemaType, NumberSchema, SchemaResponse, Property, Response, Schema } from '@azure-tools/codemodel';
+import { length, values } from '@azure-tools/linq';
+
+// The transformer adds Go-specific information to the code model.
+export async function transform(host: Host) {
+  const debug = await host.GetValue('debug') || false;
+
+  try {
+    const session = await startSession<CodeModel>(host, {}, codeModelSchema);
+
+    await process(session);
+
+    // output the model to the pipeline
+    host.WriteFile('code-model-v4.yaml', serialize(session.model), undefined, 'code-model-v4');
+
+  } catch (E) {
+    if (debug) {
+      console.error(`${__filename} - FAILURE  ${JSON.stringify(E)} ${E.stack}`);
+    }
+    throw E;
+  }
+}
+
+async function process(session: Session<CodeModel>) {
+  createResponseTypes(session);
+  // fix up struct field types
+  for (const obj of values(session.model.schemas.objects)) {
+    for (const prop of values(obj.properties)) {
+      const details = <Language>prop.schema.language.go;
+      details.name = `${schemaTypeToGoType(prop.schema)}`;
+    }
+  }
+}
+
+function schemaTypeToGoType(schema: Schema): string {
+  switch (schema.type) {
+    case SchemaType.Array:
+      return `[]${(<ArraySchema>schema).elementType.language.go!.name}`;
+    case SchemaType.Boolean:
+      return 'bool';
+    case SchemaType.ByteArray:
+      return '[]byte';
+    case SchemaType.Date:
+    case SchemaType.DateTime:
+      return 'time.Time';
+    case SchemaType.Duration:
+      return 'time.Duration';
+    case SchemaType.Integer:
+      if ((<NumberSchema>schema).precision === 32) {
+        return 'int32';
+      }
+      return 'int64';
+    case SchemaType.Number:
+      if ((<NumberSchema>schema).precision === 32) {
+        return 'float32';
+      }
+      return 'float64';
+    case SchemaType.String:
+    case SchemaType.Uuid:
+      return 'string';
+    default:
+      return schema.language.go!.name;
+  }
+}
+
+// creates the response type to be returned from an operation and updates the operation
+function createResponseTypes(session: Session<CodeModel>) {
+  for (const group of values(session.model.operationGroups)) {
+    for (const op of values(group.operations)) {
+      if (!op.responses) {
+        continue;
+      }
+      if (length(op.responses) > 1) {
+        throw console.error('multiple responses NYI');
+      }
+      // create the `type FooResponse struct` response
+      // type with a `StatusCode int` field
+      const resp = op.responses[0];
+      const name = `${op.language.go!.name}Response`;
+      resp.language.go = {
+        name: name,
+        description: `${name} contains the response from method ${group.language.go!.name}.${op.language.go!.name}.`,
+        properties: [
+          newProperty('StatusCode', 'StatusCode contains the HTTP status code.', newNumber('int', 'TODO', SchemaType.Integer, 32))
+        ]
+      }
+      // if the response defines a schema then add it as a field to the reponse type
+      if (isSchemaResponse(resp)) {
+        resp.schema.language.go!.name = schemaTypeToGoType(resp.schema);
+        (<Array<Property>>resp.language.go!.properties).push(newProperty('Value', resp.schema.language.go!.description, resp.schema));
+      }
+    }
+  }
+}
+
+function newNumber(name: string, desc: string, type: SchemaType.Integer | SchemaType.Number, precisioin: number): NumberSchema {
+  let num = new NumberSchema(name, desc, type, precisioin);
+  num.language.go = num.language.default;
+  return num;
+}
+
+function newProperty(name: string, desc: string, schema: Schema): Property {
+  let prop = new Property(name, desc, schema);
+  prop.language.go = prop.language.default;
+  return prop;
+}
+
+function isSchemaResponse(resp?: Response): resp is SchemaResponse {
+  return (resp as SchemaResponse).schema !== undefined;
+}
