3ab331c560759b8219cd9edf4d2f28d1f8176826
catalinaperalta <catalinaperaltah@hotmail.com>
2020-01-28 14:26:57 -0800

    Adding capitalizeAcronyms function in namer.ts (#215)
    * Adding capitalizeAcronyms function in namer.ts
    
    
    Co-authored-by: cperaltah <55994652+cperaltah@users.noreply.github.com>

--- src/namer/namer.ts
+++ src/namer/namer.ts
@@ -41,31 +41,36 @@ async function process(session: Session<CodeModel>) {
   // copy all the .language.default data into .language.go
   cloneLanguageInfo(model);
 
-  // pascal-case names of objects and their fields
+  // pascal-case and capitzalize acronym names of objects and their fields
   for (const obj of values(model.schemas.objects)) {
     const details = <Language>obj.language.go;
     details.name = pascalCase(details.name);
+    details.name = capitalizeAcronyms(details.name)
     for (const prop of values(obj.properties)) {
       const details = <Language>prop.language.go;
       details.name = pascalCase(details.name);
+      details.name = capitalizeAcronyms(details.name)
     }
   }
 
-  // pascal-case operation groups and their operations
+  // pascal-case and capitzalize acronym operation groups and their operations
   for (const group of values(model.operationGroups)) {
     const details = <Language>group.language.go;
     details.name = pascalCase(details.name);
+    details.name = capitalizeAcronyms(details.name)
     for (const op of values(group.operations)) {
       const details = <Language>op.language.go;
       details.name = pascalCase(details.name);
+      details.name = capitalizeAcronyms(details.name)
     }
   }
 
-  // fix up enum type and value names
+  // fix up enum type and value names and capitzalize acronyms
   for (const enm of values(session.model.schemas.choices)) {
     for (const choice of values(enm.choices)) {
       const details = <Language>choice.language.go;
       details.name = `${enm.language.go?.name}${pascalCase(details.name.toLowerCase())}`;
+      details.name = `${enm.language.go?.name}${capitalizeAcronyms(details.name)}`;
     }
   }
   return session;
@@ -78,4 +83,55 @@ function cloneLanguageInfo(graph: any) {
       instance.go = clone(instance.default, false, undefined, undefined, ['schema', 'origin']);
     }
   }
+}
+
+const acronyms = [
+  'Acl',
+  'Api',
+  'Ascii',
+  'Cpu',
+  'Css',
+  'Dns',
+  'Eof',
+  'Guid',
+  'Html',
+  'Http',
+  'Https',
+  'Id',
+  'Ip',
+  'Json',
+  'Lhs',
+  'Qps',
+  'Ram',
+  'Rfc', // TODO check
+  'Rhs',
+  'Rpc',
+  'Sla',
+  'Smtp',
+  'Sql',
+  'Ssh',
+  'Tcp',
+  'Tls',
+  'Ttl',
+  'Udp',
+  'Ui',
+  'Uid',
+  'Uuid',
+  'Uri',
+  'Url',
+  'Utf8',
+  'Vm',
+  'Xml',
+  'Xsrf',
+  'Xss'
+]
+
+// make sure that common acronyms are capitalized
+// NOTE: this function does not perform a case insensitive check considering scenarios where this would cause problems
+// for example 'curl' would end up as 'cURL' if we did case insensitive checks
+function capitalizeAcronyms(name: string): string {
+  for (const word of acronyms) {
+    name = name.replace(word, word.toUpperCase())
+  }
+  return name
 }
