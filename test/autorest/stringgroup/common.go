309f456e9b13f178a116b188f6e7b6a9b148d3e9
Catalina Peralta <caperal@microsoft.com>
2020-02-11 15:16:03 -0800

    Separating stringgroup tests and adding enum tests

---
+++ test/autorest/stringgroup/common.go
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package stringgrouptest
+
+import (
+	"reflect"
+	"testing"
+)
+
+func deepEqualOrFatal(t *testing.T, result interface{}, expected interface{}) {
+	if !reflect.DeepEqual(result, expected) {
+		t.Fatalf("got %+v, want %+v", result, expected)
+	}
+}
