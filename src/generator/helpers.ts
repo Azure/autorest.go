72e6514b2076759b78e390813d1036769f256aa3
Joel Hendrix <jhendrix@microsoft.com>
2020-03-26 08:05:11 -0700

    simplify some null checks

--- src/generator/helpers.ts
+++ src/generator/helpers.ts
@@ -169,7 +169,7 @@ export function generateParameterInfo(op: Operation): ParamInfo[] {
     const global = param.implementation === ImplementationLocation.Client;
     let isHost = false;
     if (global) {
-      isHost = (param.extensions ? (param.extensions!['x-ms-priority'] === 0 && param.extensions!['x-in'] === 'path') : false);
+      isHost = param.extensions?.['x-ms-priority'] === 0 && param.extensions?.['x-in'] === 'path';
     }
     params.push(new paramInfo(param.language.go!.name, param.schema.language.go!.name, global, param.required === true, isHost));
   }
