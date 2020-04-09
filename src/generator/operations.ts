2d35061b79db10c4c25d5c08c9c93bf9b8cd507b
Catalina Peralta <caperal@microsoft.com>
2020-04-08 19:03:43 -0700

    Moving op.responses access until after the 0 response check

--- src/generator/operations.ts
+++ src/generator/operations.ts
@@ -513,7 +513,6 @@ function isArrayOfRFC1123(schema: Schema): boolean {
 function createProtocolResponse(client: string, op: Operation, imports: ImportManager): string {
   const info = <OperationNaming>op.language.go!;
   const name = info.protocolNaming.responseMethod;
-  const firstResp = op.responses![0];
   let text = `${comment(name, '// ')} handles the ${info.name} response.\n`;
   text += `func (client *${client}) ${name}(resp *azcore.Response) (${genereateReturnsInfo(op, true).join(', ')}) {\n`;
   if (!op.responses) {
@@ -521,6 +520,7 @@ function createProtocolResponse(client: string, op: Operation, imports: ImportMa
     text += '}\n\n';
     return text;
   }
+  const firstResp = op.responses![0];
   text += `\tif !resp.HasStatusCode(${formatStatusCodes(firstResp.protocol.http?.statusCodes)}) {\n`;
   // if the response doesn't define a 'default' section return a generic error
   // TODO: can be multiple exceptions when x-ms-error-response is in use (rare)
