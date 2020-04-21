cb7eb5974d159200da7c81ba71fe025940e0d4a4
Catalina Peralta <caperal@microsoft.com>
2020-04-21 13:06:03 -0700

    adding generated code

--- test/autorest/generated/paginggroup/paging.go
+++ test/autorest/generated/paginggroup/paging.go
@@ -28,8 +28,8 @@ type PagingOperations interface {
 	GetMultiplePagesFragmentNextLink(apiVersion string, tenant string) (OdataProductResultPager, error)
 	// GetMultiplePagesFragmentWithGroupingNextLink - A paging operation that doesn't return a full URL, just a fragment with parameters grouped
 	GetMultiplePagesFragmentWithGroupingNextLink(apiVersion string, tenant string) (OdataProductResultPager, error)
-	// GetMultiplePagesLro - A long-running paging operation that includes a nextLink that has 10 pages
-	GetMultiplePagesLro(options *PagingGetMultiplePagesLroOptions) (ProductResultPager, error)
+	// BeginGetMultiplePagesLro - A long-running paging operation that includes a nextLink that has 10 pages
+	BeginGetMultiplePagesLro(options *PagingGetMultiplePagesLroOptions) (ProductResultPager, error)
 	// GetMultiplePagesRetryFirst - A paging operation that fails on the first call with 500 and then retries and then get a response including a nextLink that has 10 pages
 	GetMultiplePagesRetryFirst() (ProductResultPager, error)
 	// GetMultiplePagesRetrySecond - A paging operation that includes a nextLink that has 10 pages, of which the 2nd call fails first with 500. The client should retry and finish all 10 pages eventually.
