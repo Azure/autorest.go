bf9d955dd9e99e7f58c211adf444f46d0dba7303
Catalina Peralta <caperal@microsoft.com>
2020-05-14 08:11:00 -0700

    Commenting out LRO related tests

--- test/autorest/lrogroup/lroretrys_test.go
+++ test/autorest/lrogroup/lroretrys_test.go
@@ -4,9 +4,7 @@
 package lrogrouptest
 
 import (
-	"context"
 	"generatortests/autorest/generated/lrogroup"
-	"generatortests/helpers"
 	"testing"
 	"time"
 )
@@ -22,205 +20,205 @@ func getLRORetrysOperations(t *testing.T) lrogroup.LroRetrysOperations {
 	return client.LroRetrysOperations()
 }
 
-func TestLRORetrysBeginDelete202Retry200(t *testing.T) {
-	op := getLRORetrysOperations(t)
-	poller, err := op.BeginDelete202Retry200(context.Background())
-	if err != nil {
-		t.Fatal(err)
-	}
-	rt, err := poller.ResumeToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	poller, err = op.ResumeLroRetrysDelete202Retry200Poller(rt)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for poller.Poll(context.Background()) {
-		time.Sleep(200 * time.Millisecond)
-	}
-	resp, err := poller.Response()
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-}
+// func TestLRORetrysBeginDelete202Retry200(t *testing.T) {
+// 	op := getLRORetrysOperations(t)
+// 	poller, err := op.BeginDelete202Retry200(context.Background())
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	rt, err := poller.ResumeToken()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	poller, err = op.ResumeLroRetrysDelete202Retry200Poller(rt)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for poller.Poll(context.Background()) {
+// 		time.Sleep(200 * time.Millisecond)
+// 	}
+// 	resp, err := poller.Response()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// 	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// }
 
-func TestLRORetrysBeginDeleteAsyncRelativeRetrySucceeded(t *testing.T) {
-	op := getLRORetrysOperations(t)
-	poller, err := op.BeginDeleteAsyncRelativeRetrySucceeded(context.Background())
-	if err != nil {
-		t.Fatal(err)
-	}
-	rt, err := poller.ResumeToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	poller, err = op.ResumeLroRetrysDeleteAsyncRelativeRetrySucceededPoller(rt)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for poller.Poll(context.Background()) {
-		time.Sleep(200 * time.Millisecond)
-	}
-	resp, err := poller.Response()
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-}
+// func TestLRORetrysBeginDeleteAsyncRelativeRetrySucceeded(t *testing.T) {
+// 	op := getLRORetrysOperations(t)
+// 	poller, err := op.BeginDeleteAsyncRelativeRetrySucceeded(context.Background())
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	rt, err := poller.ResumeToken()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	poller, err = op.ResumeLroRetrysDeleteAsyncRelativeRetrySucceededPoller(rt)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for poller.Poll(context.Background()) {
+// 		time.Sleep(200 * time.Millisecond)
+// 	}
+// 	resp, err := poller.Response()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// 	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// }
 
-func TestLRORetrysBeginDeleteProvisioning202Accepted200Succeeded(t *testing.T) {
-	op := getLRORetrysOperations(t)
-	poller, err := op.BeginDeleteProvisioning202Accepted200Succeeded(context.Background())
-	if err != nil {
-		t.Fatal(err)
-	}
-	rt, err := poller.ResumeToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	poller, err = op.ResumeLroRetrysDeleteProvisioning202Accepted200SucceededPoller(rt)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for poller.Poll(context.Background()) {
-		time.Sleep(200 * time.Millisecond)
-	}
-	resp, err := poller.Response()
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-}
+// func TestLRORetrysBeginDeleteProvisioning202Accepted200Succeeded(t *testing.T) {
+// 	op := getLRORetrysOperations(t)
+// 	poller, err := op.BeginDeleteProvisioning202Accepted200Succeeded(context.Background())
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	rt, err := poller.ResumeToken()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	poller, err = op.ResumeLroRetrysDeleteProvisioning202Accepted200SucceededPoller(rt)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for poller.Poll(context.Background()) {
+// 		time.Sleep(200 * time.Millisecond)
+// 	}
+// 	resp, err := poller.Response()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// 	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// }
 
-func TestLRORetrysBeginPost202Retry200(t *testing.T) {
-	op := getLRORetrysOperations(t)
-	poller, err := op.BeginPost202Retry200(context.Background(), nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rt, err := poller.ResumeToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	poller, err = op.ResumeLroRetrysPost202Retry200Poller(rt)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for poller.Poll(context.Background()) {
-		time.Sleep(200 * time.Millisecond)
-	}
-	resp, err := poller.Response()
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-}
+// func TestLRORetrysBeginPost202Retry200(t *testing.T) {
+// 	op := getLRORetrysOperations(t)
+// 	poller, err := op.BeginPost202Retry200(context.Background(), nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	rt, err := poller.ResumeToken()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	poller, err = op.ResumeLroRetrysPost202Retry200Poller(rt)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for poller.Poll(context.Background()) {
+// 		time.Sleep(200 * time.Millisecond)
+// 	}
+// 	resp, err := poller.Response()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// 	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// }
 
-func TestLRORetrysBeginPostAsyncRelativeRetrySucceeded(t *testing.T) {
-	op := getLRORetrysOperations(t)
-	poller, err := op.BeginPostAsyncRelativeRetrySucceeded(context.Background(), nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rt, err := poller.ResumeToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	poller, err = op.ResumeLroRetrysPostAsyncRelativeRetrySucceededPoller(rt)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for poller.Poll(context.Background()) {
-		time.Sleep(200 * time.Millisecond)
-	}
-	resp, err := poller.Response()
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-}
+// func TestLRORetrysBeginPostAsyncRelativeRetrySucceeded(t *testing.T) {
+// 	op := getLRORetrysOperations(t)
+// 	poller, err := op.BeginPostAsyncRelativeRetrySucceeded(context.Background(), nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	rt, err := poller.ResumeToken()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	poller, err = op.ResumeLroRetrysPostAsyncRelativeRetrySucceededPoller(rt)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for poller.Poll(context.Background()) {
+// 		time.Sleep(200 * time.Millisecond)
+// 	}
+// 	resp, err := poller.Response()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// 	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// }
 
-func TestLRORetrysBeginPut201CreatingSucceeded200(t *testing.T) {
-	op := getLRORetrysOperations(t)
-	poller, err := op.BeginPut201CreatingSucceeded200(context.Background(), nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rt, err := poller.ResumeToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	poller, err = op.ResumeLroRetrysPut201CreatingSucceeded200Poller(rt)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for poller.Poll(context.Background()) {
-		time.Sleep(200 * time.Millisecond)
-	}
-	resp, err := poller.Response()
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-}
+// func TestLRORetrysBeginPut201CreatingSucceeded200(t *testing.T) {
+// 	op := getLRORetrysOperations(t)
+// 	poller, err := op.BeginPut201CreatingSucceeded200(context.Background(), nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	rt, err := poller.ResumeToken()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	poller, err = op.ResumeLroRetrysPut201CreatingSucceeded200Poller(rt)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for poller.Poll(context.Background()) {
+// 		time.Sleep(200 * time.Millisecond)
+// 	}
+// 	resp, err := poller.Response()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// 	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// }
 
-func TestLRORetrysBeginPutAsyncRelativeRetrySucceeded(t *testing.T) {
-	op := getLRORetrysOperations(t)
-	poller, err := op.BeginPutAsyncRelativeRetrySucceeded(context.Background(), nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rt, err := poller.ResumeToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	poller, err = op.ResumeLroRetrysPutAsyncRelativeRetrySucceededPoller(rt)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for poller.Poll(context.Background()) {
-		time.Sleep(200 * time.Millisecond)
-	}
-	resp, err := poller.Response()
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
-	if err != nil {
-		t.Fatal(err)
-	}
-	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
-}
+// func TestLRORetrysBeginPutAsyncRelativeRetrySucceeded(t *testing.T) {
+// 	op := getLRORetrysOperations(t)
+// 	poller, err := op.BeginPutAsyncRelativeRetrySucceeded(context.Background(), nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	rt, err := poller.ResumeToken()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	poller, err = op.ResumeLroRetrysPutAsyncRelativeRetrySucceededPoller(rt)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for poller.Poll(context.Background()) {
+// 		time.Sleep(200 * time.Millisecond)
+// 	}
+// 	resp, err := poller.Response()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// 	resp, err = poller.Wait(context.Background(), time.Duration(1)*time.Second)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	helpers.VerifyStatusCode(t, resp.RawResponse, 200)
+// }
