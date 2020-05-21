389ecaa9f676d90b09df5ff07b058b2df03024ae
Dapeng Zhang <dapzhang@microsoft.com>
2020-05-21 19:25:47 +0800

    Fix unexpected changes

--- test/src/tests/generated/paginggroup/models.go
+++ test/src/tests/generated/paginggroup/models.go
@@ -8,12 +8,11 @@ package paginggroup
 
 import (
 	"context"
-	"net/http"
-
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/Azure/go-autorest/tracing"
+	"net/http"
 )
 
 // The package's fully qualified name.
