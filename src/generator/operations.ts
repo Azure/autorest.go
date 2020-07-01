9549f2e80cb5e7be855796493aa626ed74cd2111
Joel Hendrix <jhendrix@microsoft.com>
2020-07-01 09:22:12 -0700

    refactor host URL replacement condition

--- src/generator/operations.ts
+++ src/generator/operations.ts
@@ -427,8 +427,10 @@ function createProtocolRequest(client: string, op: Operation, imports: ImportMan
   let parseVar = '';
   // hostURLStr will be used to check for the x-ms-parameterized-host functionality
   const hostURLStr = <string>op.requests![0].protocol.http!.uri;
-  // for parameterized hosts that are in the format "{x}" will be ignored and the client url will be used
-  if (!((hostURLStr.match(/{/g) || []).length === 1 && (hostURLStr.match(/}/g) || []).length === 1 && hostURLStr.indexOf('{') === 0 && hostURLStr.indexOf('}') === hostURLStr.length - 1)) {
+  // parameterized hosts that are in the format "{x}" will be ignored and the client url will be used.
+  // any path like "{host}/some/path" will require host path replacement.
+  // cases like "{accountName}{host}" prevent simply checking the beginning/ending of the host url.
+  if ((hostURLStr.match(/}/g) || []).length > 1 || hostURLStr[hostURLStr.length - 1] !== '}') {
     text += `\thost := "${hostURLStr}"\n`;
     // replace url parameters
     for (const pp of values(aggregateParameters(op)).where((each: Parameter) => { return each.protocol.http !== undefined && each.protocol.http!.in === 'uri'; })) {
