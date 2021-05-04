ee3d823c81267b0557d3bd573d2352f3ff76213d
Joel Hendrix <jhendrix@microsoft.com>
2021-05-04 11:28:35 -0700

    Add go mod tidy check to CI (#616)
    * Add go mod tidy check to CI
    
    Update Go to latest versions
    
    * write results of diff
    
    * add modtidy script
    
    * updated go.sum files

---
+++ .scripts/modtidy.js
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+recursiveFindGoMod('test');
+
+function recursiveFindGoMod(cur) {
+    const dir = fs.opendirSync(cur);
+    while (true) {
+        const dirEnt = dir.readSync()
+        if (dirEnt === null) {
+            break;
+        }
+        if (dirEnt.isFile() && dirEnt.name === 'go.mod') {
+            console.log('go mod tidy ' + cur);
+            execSync('go mod tidy', { cwd: cur });
+        } else if (dirEnt.isDirectory()) {
+            recursiveFindGoMod(cur + '/' + dirEnt.name);
+        }
+    }
+    dir.close();
+}
