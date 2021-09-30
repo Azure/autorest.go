e54afdb6806ce3652738e3d119592ac2c9f6dbdd
Joel Hendrix <jhendrix@microsoft.com>
2021-09-29 19:05:56 -0700

    Add test coverage for uploading files (#702)

---
+++ test/autorest/binarygroup/binarygroup_test.go
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package binarygroup
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/streaming"
+)
+
+func newBinaryGroupClient() *UploadClient {
+	return NewUploadClient(NewDefaultConnection(nil))
+}
+
+func TestBinary(t *testing.T) {
+	client := newBinaryGroupClient()
+	resp, err := client.Binary(context.Background(), streaming.NopCloser(bytes.NewReader([]byte{0xff, 0xfe, 0xfd})), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sc := resp.RawResponse.StatusCode; sc != http.StatusOK {
+		t.Fatalf("unexpected status code %d", sc)
+	}
+}
+
+func TestFile(t *testing.T) {
+	client := newBinaryGroupClient()
+	jsonFile := strings.NewReader(`{ "more": "cowbell" }`)
+	resp, err := client.File(context.Background(), streaming.NopCloser(jsonFile), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sc := resp.RawResponse.StatusCode; sc != http.StatusOK {
+		t.Fatalf("unexpected status code %d", sc)
+	}
+}
