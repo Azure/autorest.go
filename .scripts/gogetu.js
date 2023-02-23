b0e7313c9f6575b3fccdc34fb558f31f5045c9b4
Joel Hendrix <jhendrix@microsoft.com>
2023-02-23 11:42:00 -0800

    Update dependencies
    Added rush command gogetu to automate the process.

---
+++ .scripts/gogetu.js
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+recursiveUpdateGoMod('test');
+
+function recursiveUpdateGoMod(cur) {
+    const dir = fs.opendirSync(cur);
+    while (true) {
+        const dirEnt = dir.readSync()
+        if (dirEnt === null) {
+            break;
+        }
+        if (dirEnt.isFile() && dirEnt.name === 'go.mod') {
+            console.log('go get -u ' + cur);
+            execSync('go get -u', { cwd: cur });
+            console.log('go mod tidy ' + cur);
+            execSync('go mod tidy', { cwd: cur });
+        } else if (dirEnt.isDirectory()) {
+            recursiveUpdateGoMod(cur + '/' + dirEnt.name);
+        }
+    }
+    dir.close();
+}
