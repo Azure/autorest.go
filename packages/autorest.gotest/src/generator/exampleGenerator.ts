9a175531cc48a10ca35f15abc134b3df21d3d4f2
tadelesh <tadelesh.shi@live.cn>
2023-03-20 18:12:05 +0800

    format code

--- packages/autorest.gotest/src/generator/exampleGenerator.ts
+++ packages/autorest.gotest/src/generator/exampleGenerator.ts
@@ -8,8 +8,7 @@ import { Config } from '../common/constant';
 import { ExampleModel, MockTestDefinitionModel } from '@autorest/testmodeler/dist/src/core/model';
 import { MockTestDataRender } from './mockTestGenerator';
 import { ParameterOutput } from '../common/model';
-import { camelCase, values } from 'lodash';
-import _ = require('lodash');
+import { camelCase, trimEnd } from 'lodash';
 import { ObjectSchema, Parameter, SchemaType } from '@autorest/codemodel';
 import { sortParametersByRequired } from '@autorest/go/dist/generator/helpers';
 
@@ -21,7 +20,7 @@ export class ExampleDataRender extends MockTestDataRender {
       if (group.language.go!.clientParams) {
         const clientParams = <Array<Parameter>>group.language.go!.clientParams;
         for (const clientParam of clientParams) {
-          if (allClientParams.filter(cp => cp.language.go!.name === clientParam.language.go!.name).length > 0) {
+          if (allClientParams.filter((cp) => cp.language.go!.name === clientParam.language.go!.name).length > 0) {
             continue;
           }
           allClientParams.push(clientParam);
@@ -58,13 +57,23 @@ export class ExampleCodeGenerator extends BaseCodeGenerator {
         fileName = fileName.substring(0, fileName.length - 6) + '_client';
       }
 
-      this.renderAndWrite({ clientFactoryParametersOutput: this.context.codeModel.testModel.mockTest['clientFactoryParametersOutput'], exampleGroups: exampleGroups, swaggerCommit: this.context.swaggerCommit }, 'exampleTest.go.njk', `${this.getFilePrefix(Config.exampleFilePrefix)}${fileName}_example_test.go`, extraParam, {
-        getParamsValue: this.getParamsValue,
-        getExampleSuffix: (exampleKey: string) => {
-          return camelCase(exampleKey);
+      this.renderAndWrite(
+        {
+          clientFactoryParametersOutput: this.context.codeModel.testModel.mockTest['clientFactoryParametersOutput'],
+          exampleGroups: exampleGroups,
+          swaggerCommit: this.context.swaggerCommit,
         },
-        getCommentResponseOutput: this.getCommentResponseOutput,
-      });
+        'exampleTest.go.njk',
+        `${this.getFilePrefix(Config.exampleFilePrefix)}${fileName}_example_test.go`,
+        extraParam,
+        {
+          getParamsValue: this.getParamsValue,
+          getExampleSuffix: (exampleKey: string) => {
+            return camelCase(exampleKey);
+          },
+          getCommentResponseOutput: this.getCommentResponseOutput,
+        },
+      );
     }
   }
 
@@ -91,7 +100,7 @@ export class ExampleCodeGenerator extends BaseCodeGenerator {
         indentNum++;
       }
     }
-    return _.trimEnd(result, '\n');
+    return trimEnd(result, '\n');
   }
 
   public getParamsValue(params: Array<ParameterOutput>) {
