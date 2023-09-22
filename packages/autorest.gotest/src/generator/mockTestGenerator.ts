31a6bbee6290ffadff5b0838ad46be99e0a444ec
Alancere <804873052@qq.com>
2023-09-22 17:28:34 +0800

    pager nextLink

--- packages/autorest.gotest/src/generator/mockTestGenerator.ts
+++ packages/autorest.gotest/src/generator/mockTestGenerator.ts
@@ -307,7 +307,9 @@ export class MockTestDataRender extends BaseDataRender {
         const isPolymophismValue =
           value?.schema?.type === SchemaType.Object &&
           ((<ObjectSchema>value.schema).discriminatorValue !== undefined || (<ObjectSchema>value.schema).discriminator?.property.isDiscriminator === true);
-        output += `${this.getLanguageName(value)}: ${this.exampleValueToString(value, isPolymophismValue || !value.language.go?.byValue === true)},\n`;
+          if (!this.getLanguageName(value).includes('NextLink')) {
+            output += `${this.getLanguageName(value)}: ${this.exampleValueToString(value, isPolymophismValue || !value.language.go?.byValue === true)},\n`;
+          }
       }
       output += '}';
       return output;
