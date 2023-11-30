38e8ce6480006a9d57fc90b26c25cb6fb0ed2083
Joel Hendrix <jhendrix@microsoft.com>
2023-11-30 15:14:29 -0800

    add missing terminal quote
    enable verbose flag for tspcompile

--- packages/typespec-go/.scripts/tspcompile.js
+++ packages/typespec-go/.scripts/tspcompile.js
@@ -73,7 +73,7 @@ function generate(moduleName, moduleVersion, inputDir, outputDir, additionalArgs
     additionalArgs = [];
   } else {
     for (let i = 0; i < additionalArgs.length; ++i) {
-      additionalArgs[i] = `--option="@azure-tools/typespec-go.${additionalArgs[i]}`;
+      additionalArgs[i] = `--option="@azure-tools/typespec-go.${additionalArgs[i]}"`;
     }
   }
   sem.take(function() {
