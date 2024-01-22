35ebd1d0b1e835ce0e091a4d89afdf173eaec4bd
Joel Hendrix <jhendrix@microsoft.com>
2024-01-22 13:15:05 -0800

    Add test coverage for tsp special-headers groups

---
+++ packages/typespec-go/test/cadlranch/special-headers/condreqgroup/custom_client.go
@@ -0,0 +1,22 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package condreqgroup
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+)
+
+func NewConditionalRequestClient(options *azcore.ClientOptions) (*ConditionalRequestClient, error) {
+	internal, err := azcore.NewClient("condreqgroup", "v0.1.0", runtime.PipelineOptions{}, options)
+	if err != nil {
+		return nil, err
+	}
+	return &ConditionalRequestClient{
+		internal: internal,
+	}, nil
+}
