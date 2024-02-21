1df36c1ef43b8a66686282397bbe6bfc5858d340
Joel Hendrix <jhendrix@microsoft.com>
2024-02-21 09:01:19 -0800

    Add tsp flattengroup coverage (#1149)
    We don't honor the flattenProperty decorator.

---
+++ packages/typespec-go/test/cadlranch/azure/client-generator-core/flattengroup/flatten_client_test.go
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package flattengroup_test
+
+import (
+	"context"
+	"flattengroup"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFlattenClient_PutFlattenModel(t *testing.T) {
+	client, err := flattengroup.NewFlattenClient(nil)
+	require.NoError(t, err)
+	resp, err := client.PutFlattenModel(context.Background(), flattengroup.FlattenModel{
+		Name: to.Ptr("foo"),
+		Properties: &flattengroup.ChildModel{
+			Description: to.Ptr("bar"),
+			Age:         to.Ptr[int32](10),
+		},
+	}, nil)
+	require.NoError(t, err)
+	require.Equal(t, flattengroup.FlattenModel{
+		Name: to.Ptr("test"),
+		Properties: &flattengroup.ChildModel{
+			Description: to.Ptr("test"),
+			Age:         to.Ptr[int32](1),
+		},
+	}, resp.FlattenModel)
+}
+
+func TestFlattenClient_PutNestedFlattenModel(t *testing.T) {
+	client, err := flattengroup.NewFlattenClient(nil)
+	require.NoError(t, err)
+	resp, err := client.PutNestedFlattenModel(context.Background(), flattengroup.NestedFlattenModel{
+		Name: to.Ptr("foo"),
+		Properties: &flattengroup.ChildFlattenModel{
+			Summary: to.Ptr("bar"),
+			Properties: &flattengroup.ChildModel{
+				Description: to.Ptr("test"),
+				Age:         to.Ptr[int32](10),
+			},
+		},
+	}, nil)
+	require.NoError(t, err)
+	require.Equal(t, flattengroup.NestedFlattenModel{
+		Name: to.Ptr("test"),
+		Properties: &flattengroup.ChildFlattenModel{
+			Summary: to.Ptr("test"),
+			Properties: &flattengroup.ChildModel{
+				Description: to.Ptr("foo"),
+				Age:         to.Ptr[int32](1),
+			},
+		},
+	}, resp.NestedFlattenModel)
+}
