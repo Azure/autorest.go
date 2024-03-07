40c08e0874e0579e810873c6b197651927fe39bb
Joel Hendrix <jhendrix@microsoft.com>
2024-03-07 09:14:48 -0800

    simplify arg check

--- packages/typespec-go/.scripts/cadl-ranch.js
+++ packages/typespec-go/.scripts/cadl-ranch.js
@@ -4,26 +4,21 @@ import { execSync } from 'child_process';
 
 const toolsModRoot = execSync('git rev-parse --show-toplevel').toString().trim() + '/packages/typespec-go/node_modules/@azure-tools/';
 
-const args = process.argv.slice(2);
 const switches = [];
-for (var i = 0 ; i < args.length; i += 1) {
-  switch (args[i]) {
-    case '--start':
-      switches.push('server');
-      switches.push('start');
-      switches.push(toolsModRoot + 'cadl-ranch-specs/http');
-      break;
-    case '--stop':
-      switches.push('server');
-      switches.push('stop');
-      break;
-    default:
-      break;
-  }
+switch (process.argv[2]) {
+  case '--start':
+    switches.push('server');
+    switches.push('start');
+    switches.push(toolsModRoot + 'cadl-ranch-specs/http');
+    break;
+  case '--stop':
+    switches.push('server');
+    switches.push('stop');
+    break;
 }
 
 if (switches.length === 0) {
-  throw new Error('missing args: [--start] [--stop]');
+  throw new Error('missing arg: [--start] [--stop]');
 }
 
 const cmdLine = toolsModRoot + 'cadl-ranch/node_modules/.bin/cadl-ranch ' + switches.join(' ');
