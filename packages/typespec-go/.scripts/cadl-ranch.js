0ca149460bd69d430b234bb845f2aefca0b59103
Joel Hendrix <jhendrix@microsoft.com>
2024-03-07 07:50:55 -0800

    Update to released tcgc
    Use latest dev version from npm.
    Add cadl-ranch to dev dependencies and added rush script to start
    and stop the test server.
    Removed unnecessary global installs for CI.

---
+++ packages/typespec-go/.scripts/cadl-ranch.js
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+import { execSync } from 'child_process';
+
+const toolsModRoot = execSync('git rev-parse --show-toplevel').toString().trim() + '/packages/typespec-go/node_modules/@azure-tools/';
+
+const args = process.argv.slice(2);
+const switches = [];
+for (var i = 0 ; i < args.length; i += 1) {
+  switch (args[i]) {
+    case '--start':
+      switches.push('server');
+      switches.push('start');
+      switches.push(toolsModRoot + 'cadl-ranch-specs/http');
+      break;
+    case '--stop':
+      switches.push('server');
+      switches.push('stop');
+      break;
+    default:
+      break;
+  }
+}
+
+if (switches.length === 0) {
+  throw new Error('missing args: [--start] [--stop]');
+}
+
+const cmdLine = toolsModRoot + 'cadl-ranch/node_modules/.bin/cadl-ranch ' + switches.join(' ');
+console.log(cmdLine);
+execSync(cmdLine);
