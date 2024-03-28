20c6d2c8f33b5a169168aa97c445770c3c3a4a55
Joel Hendrix <jhendrix@microsoft.com>
2024-03-28 09:08:26 -0700

    Add tsp coverage for contentneggroup (#1176)
    * Add tsp coverage for contentneggroup
    
    * simplify tests

---
+++ packages/typespec-go/test/cadlranch/payload/contentneggroup/contentnegotiation_client.go
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package contentneggroup
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+)
+
+func NewContentNegotiationClient(options *azcore.ClientOptions) (*ContentNegotiationClient, error) {
+	internal, err := azcore.NewClient("contentneggroup", "v0.1.0", runtime.PipelineOptions{}, options)
+	if err != nil {
+		return nil, err
+	}
+	return &ContentNegotiationClient{
+		internal: internal,
+	}, nil
+}
