274930bb7a759be8c9628a0329d5db658bd16ba8
JiaqiZhang-Dev <jiaqzhang@microsoft.com>
2025-02-17 09:25:34 +0800

    fix test errors

--- packages/autorest.go/src/generator/generator.ts
+++ packages/autorest.go/src/generator/generator.ts
@@ -60,7 +60,7 @@ export async function generateCode(host: AutorestExtensionHost) {
       });
     }
 
-    let factoryGatherAllParams = await session.getValue('factory-gather-all-params', false);
+    const factoryGatherAllParams = await session.getValue('factory-gather-all-params', false);
     session.model.options.factoryGatherAllParams = factoryGatherAllParams;
     const clientFactory = await generateClientFactory(session.model);
     if (clientFactory.length > 0) {
