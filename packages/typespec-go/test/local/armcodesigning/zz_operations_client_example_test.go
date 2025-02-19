350ba7489cd93c5f60e57b086fce8a02a43e02c5
JiaqiZhang-Dev <jiaqzhang@microsoft.com>
2025-02-19 11:24:45 +0800

    special case for operationsClient

--- packages/typespec-go/test/local/armcodesigning/zz_operations_client_example_test.go
+++ packages/typespec-go/test/local/armcodesigning/zz_operations_client_example_test.go
@@ -18,7 +18,7 @@ func ExampleOperationsClient_NewListPager() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	clientFactory, err := armcodesigning.NewClientFactory(cred, nil)
+	clientFactory, err := armcodesigning.NewClientFactory("<subscriptionID>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
--- packages/typespec-go/test/local/armloadtestservice/zz_client_factory.go
+++ packages/typespec-go/test/local/armloadtestservice/zz_client_factory.go
@@ -12,23 +12,20 @@ import (
 // ClientFactory is a client factory used to create any client in this module.
 // Don't use this type directly, use NewClientFactory instead.
 type ClientFactory struct {
-	subscriptionID string
-	internal       *arm.Client
+	internal *arm.Client
 }
 
 // NewClientFactory creates a new instance of ClientFactory with the specified values.
 // The parameter values will be propagated to any client created from this factory.
-//   - subscriptionID - The ID of the target subscription. The value must be an UUID.
 //   - credential - used to authorize requests. Usually a credential from azidentity.
 //   - options - pass nil to accept the default values.
-func NewClientFactory(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) (*ClientFactory, error) {
+func NewClientFactory(credential azcore.TokenCredential, options *arm.ClientOptions) (*ClientFactory, error) {
 	internal, err := arm.NewClient(moduleName, moduleVersion, credential, options)
 	if err != nil {
 		return nil, err
 	}
 	return &ClientFactory{
-		subscriptionID: subscriptionID,
-		internal:       internal,
+		internal: internal,
 	}, nil
 }
 
@@ -47,9 +44,9 @@ func (c *ClientFactory) NewLoadTestProfileMappingsClient() *LoadTestProfileMappi
 }
 
 // NewLoadTestsClient creates a new instance of LoadTestsClient.
-func (c *ClientFactory) NewLoadTestsClient() *LoadTestsClient {
+func (c *ClientFactory) NewLoadTestsClient(subscriptionID string) *LoadTestsClient {
 	return &LoadTestsClient{
-		subscriptionID: c.subscriptionID,
+		subscriptionID: subscriptionID,
 		internal:       c.internal,
 	}
 }
@@ -62,9 +59,9 @@ func (c *ClientFactory) NewOperationsClient() *OperationsClient {
 }
 
 // NewQuotasClient creates a new instance of QuotasClient.
-func (c *ClientFactory) NewQuotasClient() *QuotasClient {
+func (c *ClientFactory) NewQuotasClient(subscriptionID string) *QuotasClient {
 	return &QuotasClient{
-		subscriptionID: c.subscriptionID,
+		subscriptionID: subscriptionID,
 		internal:       c.internal,
 	}
 }
