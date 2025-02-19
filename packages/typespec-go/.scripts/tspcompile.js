6ce400d1ea7d33b6fc3c760a829568b2ba31a489
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2025-02-19 13:42:22 -0800

    Don't export pager constructors for internal methods (#1499)

--- packages/typespec-go/.scripts/tspcompile.js
+++ packages/typespec-go/.scripts/tspcompile.js
@@ -166,6 +166,9 @@ generate('azkeys', azkeys, 'test/local/azkeys');
 const armtest = pkgRoot + 'test/tsp/Test.Management';
 generate('armtest', armtest, 'test/local/armtest');
 
+const internalpager = pkgRoot + 'test/tsp/Internal.Pager';
+generate('internalpager', internalpager, 'test/local/internalpager', ['generate-fakes=false']);
+
 loopSpec(httpSpecsGroup, httpSpecs, 'test/http-specs')
 loopSpec(azureHttpSpecsGroup, azureHttpSpecs, 'test/azure-http-specs')
 
