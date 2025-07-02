7f24f67f777815dca2298703d0a031edf48b7f0e
Judy Liu <v-liujudy@microsoft.com>
2025-07-02 09:54:10 +0800

    Add go test2 (#1566)

---
+++ packages/typespec-go/test/azure-http-specs/azure/encode/encodedurationgroup/duration_client_test.go
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package encodedurationgroup
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDurationClient_DurationConstant(t *testing.T) {
+	client, err := NewDurationClient(nil)
+	require.NoError(t, err)
+	input := "1.02:59:59.5000000"
+	body := DurationModel{
+		Input: &input,
+	}
+	result, err := client.DurationConstant(context.Background(), body, nil)
+	require.NoError(t, err)
+	require.Equal(t, DurationClientDurationConstantResponse{}, result)
+}
