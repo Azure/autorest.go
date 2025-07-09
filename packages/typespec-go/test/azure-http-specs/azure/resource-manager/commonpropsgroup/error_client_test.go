578d3cd2fc32576471071504994d5b264585f2fb
ReleaseHelper <ReleaseHelper>
2025-07-09 14:00:34 +0800

    update test

--- packages/typespec-go/test/azure-http-specs/azure/resource-manager/commonpropsgroup/error_client_test.go
+++ packages/typespec-go/test/azure-http-specs/azure/resource-manager/commonpropsgroup/error_client_test.go
@@ -5,25 +5,13 @@ package commonpropsgroup_test
 
 import (
 	"commonpropsgroup"
-	"encoding/json"
 	"net/http"
-	"regexp"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/stretchr/testify/require"
 )
-
-type ErrorResp struct {
-	Code       string     `json:"code"`
-	Message    string     `json:"message"`
-	InnerError InnerError `json:"innererror"`
-}
-
-type InnerError struct {
-	ExceptionType string `json:"exceptiontype"`
-}
 
 func TestErrorClient_CreateForUserDefinedError(t *testing.T) {
 	resp, err := clientFactory.NewErrorClient().CreateForUserDefinedError(ctx, resourceGroupExpected, "confidential", commonpropsgroup.ConfidentialResource{
@@ -34,14 +22,10 @@ func TestErrorClient_CreateForUserDefinedError(t *testing.T) {
 	}, nil)
 	require.Error(t, err)
 	require.NotNil(t, resp)
-	re := regexp.MustCompile(`(?s)\{.*\}`)
-	jsonStr := re.FindString(err.Error())
-	var errorResp ErrorResp
-	err = json.Unmarshal([]byte(jsonStr), &errorResp)
-	require.NoError(t, err)
-	require.Contains(t, errorResp.Message, "Username should not contain only numbers")
-	require.Equal(t, "general", errorResp.InnerError.ExceptionType)
-	require.Equal(t, "BadRequest", errorResp.Code)
+	var respErr *azcore.ResponseError
+	require.ErrorAs(t, err, &respErr)
+	require.EqualValues(t, http.StatusBadRequest, respErr.StatusCode)
+	require.Zero(t, resp)
 }
 
 func TestErrorClient_GetForPredefinedError(t *testing.T) {
