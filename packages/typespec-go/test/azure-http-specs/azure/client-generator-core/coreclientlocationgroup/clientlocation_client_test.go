b28dc12328ae8159255c83f15a1fd04b08a13d39
ReleaseHelper <ReleaseHelper>
2025-07-10 17:32:07 +0800

    add test

---
+++ packages/typespec-go/test/azure-http-specs/azure/client-generator-core/coreclientlocationgroup/clientlocation_client_test.go
@@ -0,0 +1,17 @@
+package coreclientlocationgroup
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestClientLocationClient_GetHealthStatus(t *testing.T) {
+	client, err := NewClientLocationClient(nil)
+	require.NoError(t, err)
+	require.NotNil(t, client)
+	resp, err := client.GetHealthStatus(context.Background(), nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+}
