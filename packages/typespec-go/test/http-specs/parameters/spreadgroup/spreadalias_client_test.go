0b0e2824dd0ede0c36f2d5597f024077cf6703fd
Joel Hendrix <jhendrix@microsoft.com>
2025-08-27 07:46:49 -0700

    fix tests after rebase

--- packages/typespec-go/test/http-specs/parameters/spreadgroup/spreadalias_client_test.go
+++ packages/typespec-go/test/http-specs/parameters/spreadgroup/spreadalias_client_test.go
@@ -40,14 +40,14 @@ func TestSpreadAliasClient_SpreadWithMultipleParameters(t *testing.T) {
 }
 
 func TestSpreadAliasClient_SpreadWithInnerAlias(t *testing.T) {
-	client, err := spreadgroup.NewSpreadClient("http://localhost:3000", nil)
+	client, err := spreadgroup.NewSpreadClientWithNoCredential("http://localhost:3000", nil)
 	require.NoError(t, err)
 	_, err = client.NewSpreadAliasClient().SpreadParameterWithInnerAlias(context.Background(), "1", "foo", 1, "bar", nil)
 	require.NoError(t, err)
 }
 
 func TestSpreadAliasClient_SpreadWithInnerModel(t *testing.T) {
-	client, err := spreadgroup.NewSpreadClient("http://localhost:3000", nil)
+	client, err := spreadgroup.NewSpreadClientWithNoCredential("http://localhost:3000", nil)
 	require.NoError(t, err)
 	_, err = client.NewSpreadAliasClient().SpreadParameterWithInnerModel(context.Background(), "1", "foo", "bar", nil)
 	require.NoError(t, err)
