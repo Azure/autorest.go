55dbe0ee275d0e9dc8388b08c800adf95b071048
Judy Liu <v-liujudy@microsoft.com>
2025-09-19 17:10:44 +0800

    add test

---
+++ packages/typespec-go/test/azure-http-specs/azure/resource-manager/methodsubscriptionidgroup/subscriptionresource1operations_client_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package methodsubscriptionidgroup_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/stretchr/testify/require"
+
+	"methodsubscriptionidgroup"
+)
+
+func TestSubscriptionResource1OperationsClient_CRUD(t *testing.T) {
+	subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+	require.NotEmpty(t, subscriptionID, "AZURE_SUBSCRIPTION_ID must be set")
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	require.NoError(t, err)
+
+	client, err := methodsubscriptionidgroup.NewSubscriptionResource1OperationsClient(subscriptionID, cred, nil)
+	require.NoError(t, err)
+	require.NotNil(t, client)
+
+	resourceName := "test-resource1"
+	resource := methodsubscriptionidgroup.SubscriptionResource1{
+		// TODO: Fill with required fields for creation according to Azure best practices
+	}
+
+	// Create (Put)
+	putResp, err := client.Put(context.Background(), resourceName, resource, nil)
+	require.NoError(t, err)
+	require.NotNil(t, putResp.SubscriptionResource1)
+
+	// Get
+	getResp, err := client.Get(context.Background(), resourceName, nil)
+	require.NoError(t, err)
+	require.NotNil(t, getResp.SubscriptionResource1)
+
+	// Delete
+	delResp, err := client.Delete(context.Background(), resourceName, nil)
+	require.NoError(t, err)
+	require.NotNil(t, delResp)
+}
