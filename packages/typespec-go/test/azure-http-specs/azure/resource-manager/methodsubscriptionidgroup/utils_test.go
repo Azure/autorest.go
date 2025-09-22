50ac1abf444d43f6c373512d18f1d87ce641f0a8
Judy Liu <v-liujudy@microsoft.com>
2025-09-22 17:06:22 +0800

    fix test

--- packages/typespec-go/test/azure-http-specs/azure/resource-manager/methodsubscriptionidgroup/utils_test.go
+++ packages/typespec-go/test/azure-http-specs/azure/resource-manager/methodsubscriptionidgroup/utils_test.go
@@ -21,7 +21,6 @@ var (
 	clientFactory *methodsubscriptionidgroup.ClientFactory
 
 	subscriptionIdExpected = "00000000-0000-0000-0000-000000000000"
-	locationExpected       = "eastus"
 )
 
 func TestMain(m *testing.M) {
