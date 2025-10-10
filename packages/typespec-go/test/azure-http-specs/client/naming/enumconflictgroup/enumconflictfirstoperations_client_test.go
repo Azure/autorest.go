5492927b80ba3403f418db3ad017b00efe3096f6
Judy Liu <v-liujudy@microsoft.com>
2025-10-10 15:24:45 +0800

    add enumconflivt test

---
+++ packages/typespec-go/test/azure-http-specs/client/naming/enumconflictgroup/enumconflictfirstoperations_client_test.go
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package enumconflictgroup_test
+
+import (
+	"context"
+	"testing"
+
+	"enumconflictgroup"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEnumConflictFirstOperationsClient_First(t *testing.T) {
+	enumClient, err := enumconflictgroup.NewEnumConflictClientWithNoCredential("http://localhost:3000", nil)
+	require.NoError(t, err)
+
+	status := enumconflictgroup.Status("active")
+	body := enumconflictgroup.FirstModel{
+		Status: &status,
+		Name:   to.Ptr("test"),
+	}
+
+	resp, err := enumClient.NewEnumConflictFirstOperationsClient().First(context.Background(), body, nil)
+	require.NoError(t, err)
+	require.Equal(t, body.Status, resp.FirstModel.Status)
+	require.Equal(t, body.Name, resp.FirstModel.Name)
+}
---
+++ packages/typespec-go/test/azure-http-specs/client/naming/enumconflictgroup/enumconflictsecondoperations_client_test.go
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package enumconflictgroup_test
+
+import (
+	"context"
+	"testing"
+
+	"enumconflictgroup"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEnumConflictSecondOperationsClient_Second(t *testing.T) {
+	enumClient, err := enumconflictgroup.NewEnumConflictClientWithNoCredential("http://localhost:3000", nil)
+	require.NoError(t, err)
+
+	status := enumconflictgroup.SecondStatus("running")
+	body := enumconflictgroup.SecondModel{
+		Status:      &status,
+		Description: to.Ptr("test description"),
+	}
+
+	resp, err := enumClient.NewEnumConflictSecondOperationsClient().Second(context.Background(), body, nil)
+	require.NoError(t, err)
+	require.Equal(t, body.Status, resp.SecondModel.Status)
+	require.Equal(t, body.Description, resp.SecondModel.Description)
+}
