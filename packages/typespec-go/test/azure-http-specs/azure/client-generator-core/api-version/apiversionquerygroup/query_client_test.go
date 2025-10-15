1b4749cc372fe2815809e2942cbeb0f44925b095
Judy Liu <v-liujudy@microsoft.com>
2025-10-15 13:31:11 +0800

    update test

--- packages/typespec-go/test/azure-http-specs/azure/client-generator-core/api-version/apiversionquerygroup/query_client_test.go
+++ packages/typespec-go/test/azure-http-specs/azure/client-generator-core/api-version/apiversionquerygroup/query_client_test.go
@@ -4,19 +4,19 @@
 package apiversionquerygroup_test
 
 import (
-    "context"
-    "testing"
+	"context"
+	"testing"
 
-    "apiversionquerygroup"
-    "github.com/stretchr/testify/require"
+	"apiversionquerygroup"
+	"github.com/stretchr/testify/require"
 )
 
 func TestQueryClient_QueryAPIVersion(t *testing.T) {
-    client, err := apiversionquerygroup.NewQueryClientWithNoCredential("http://localhost:3000", nil)
-    require.NoError(t, err)
-    require.NotNil(t, client)
+	client, err := apiversionquerygroup.NewQueryClientWithNoCredential("http://localhost:3000", nil)
+	require.NoError(t, err)
+	require.NotNil(t, client)
 
-    resp, err := client.QueryAPIVersion(context.Background(), nil)
-    require.NoError(t, err)
-    require.NotNil(t, resp)
+	resp, err := client.QueryAPIVersion(context.Background(), nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
 }
