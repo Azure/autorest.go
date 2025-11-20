c49c47303f69fc23fb371d675f0aafabfefca64a
Joel Hendrix <jhendrix@microsoft.com>
2025-11-20 10:22:21 -0800

    clean up lint

--- packages/autorest.go/test/synapse/azspark/version.go
+++ packages/autorest.go/test/synapse/azspark/version.go
@@ -1,9 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-package azartifacts
-
-type CloudErrorBody struct{}
+package azspark
 
 // suppress unused vars lint
 var _ = moduleName
